4c308eaffd00cdcd6c7d8776ca9b5f9aa57e6e04
Alex Martin <alexgm88@gmail.com>
2020-04-20 21:27:49 -0400

    added sort features

--- script.js
+++ script.js
@@ -37,6 +37,13 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
 // Add new obj to data array
 function addData(obj) {
   data.push(obj);
@@ -67,3 +74,4 @@ function formatMoney(number) {
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
